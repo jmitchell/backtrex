4c4146659a20b761035708650fea62e3ec9cb4c5
Jacob Mitchell <jmitchell@member.fsf.org>
2017-01-10 11:07:11 -0800

    initial commit

---
+++ lib/backtrex.ex
@@ -0,0 +1,18 @@
+defmodule Backtrex do
+  @moduledoc """
+  Documentation for Backtrex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Backtrex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/backtrex_test.exs
@@ -0,0 +1,8 @@
+defmodule BacktrexTest do
+  use ExUnit.Case
+  doctest Backtrex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
