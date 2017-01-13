edcec71e8df5d90cc753a4bada8d19b8141288bb
Jacob Mitchell <jmitchell@member.fsf.org>
2017-01-12 22:36:31 -0800

    Attempt to support Elixir ~> 1.3.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Backtrex.Mixfile do
   def project do
     [app: :backtrex,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      elixirc_options: [warnings_as_errors: true],
